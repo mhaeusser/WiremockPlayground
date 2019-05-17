dfff5290dd315b82c2f4ca88f702baf23df027cc
mhaeusser <mhaeusser@web.de>
2019-05-17 15:01:11 +0200

    Added class WMStartStop.

--- src/test/java/de/mhaeusser/wiremock/WMStartStop.java
+++ src/test/java/de/mhaeusser/wiremock/WMStartStop.java
@@ -1,23 +1,32 @@
 package de.mhaeusser.wiremock;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
-public class WMStarter {
+public class WMStartStop {
 
     public static final int portNumber = 8090;
 
-    public static void main(String[] args) {
+    public static final int sec = 2;
+
+    public static void main(String[] args) throws InterruptedException {
         WireMockServer wireMockServer = new WireMockServer(wireMockConfig().port(portNumber));
         wireMockServer.start();
+        diagRunning(wireMockServer);
+
+        System.out.println("Waiting " + sec + " seconds.");
+        Thread.sleep(sec * 1000);
+
+        wireMockServer.stop();
         diagRunning(wireMockServer);
     }
 
     public static void diagRunning(WireMockServer wireMockServer) {
         System.out.print("WireMockServer is ");
         if (!wireMockServer.isRunning()) System.out.print("NOT ");
-        System.out.print("running on port " + wireMockServer.port());
+        // wireMockServer.port() leads to IllegalStateException when server has been stopped
+        System.out.println("running on port " + wireMockServer.getOptions().portNumber());
     }
 }
